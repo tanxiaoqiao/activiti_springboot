c3ab892210b16893223529b3087a99e7790a0232
Xiaoqiao Tan <Xiaoqiao.Tan@Honeywell.com>
2018-07-20 14:08:13 +0800

    first

--- src/main/java/com/example/act/service/ActivityController.java
+++ src/main/java/com/example/act/service/ActivityController.java
@@ -3,6 +3,7 @@ package com.example.act.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: Kris
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @Date: 7/19/2018 3:18 PM
  */
 
-//@RestController
-//@RequestMapping("/test")
+@RestController
+@RequestMapping("/test")
 public class ActivityController {
 
     @Autowired
